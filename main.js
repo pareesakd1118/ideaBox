aa6fb5782ae29135127ef0d8eb729aba25d4630a
Asher Spurr <asher.spurr@gmail.com>
2024-01-04 17:48:05 -0700

    Creates data model for idea object, completes iteration 0

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 /*<><>Data Models<><>*/
-var savedIdeas = []
+var savedIdeas = [];
+var idea;
+
 /*<><>Query Selectors<><>*/
-var saveButton = document.querySelector('#save-button')
-var titleInput = document.querySelector('#title')
-var bodyInput = document.querySelector('#body')
+var saveButton = document.getElementById('save-button');
+var ideaForm = document.getElementById('form');
+
 /*<><><>Event Listeners<><>*/
-saveButton.addEventListener('click', saveIdea)
+ideaForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var titleInput = document.getElementById('title');
+    var bodyInput = document.getElementById('body');
+    if(titleInput.value == "" || bodyInput.value == "") {
+        alert('Error');
+    }
+    else {
+        saveIdea(titleInput.value, bodyInput.value);
+        pushIdea();
+    }
+})
+
 /*<><>Functions<><>*/
-function saveIdea() {
-    if(titleInput.value.length > 0 && bodyInput.value.length > 0) {
-        var idea = {
-            title: titleInput.value,
-            body: bodyInput.value,
-            id: Date.now(),
-        }
-    console.log(idea)
-}
+function saveIdea(title, body) {
+    idea = {
+        title: title,
+        body: body,
+        id: Date.now(),
+    }
+console.log(idea);
+return idea;
 }
 
-// function pushIdea(idea) {
-// savedIdeas.push(idea)
-// }
-
+function pushIdea() {
+    savedIdeas.push(idea);
+    console.log(savedIdeas);
+}
