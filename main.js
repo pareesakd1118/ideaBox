603319bcea2bddd6258b6329894430d35c02db02
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-01-04 21:51:50 -0500

    Add a new card to the output area using form values

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var idea;
 /*<><>Query Selectors<><>*/
 var saveButton = document.getElementById('save-button');
 var ideaForm = document.getElementById('form');
+var outputSection = document.querySelector('.output-area')
 
 /*<><><>Event Listeners<><>*/
 ideaForm.addEventListener('submit', (e) => {
@@ -17,10 +18,13 @@ ideaForm.addEventListener('submit', (e) => {
     else {
         saveIdea(titleInput.value, bodyInput.value);
         pushIdea();
+        renderCard();
+        ideaForm.reset()
     }
 })
 
 /*<><>Functions<><>*/
+//This function makes an idea object with the title 
 function saveIdea(title, body) {
     idea = {
         title: title,
@@ -31,7 +35,21 @@ console.log(idea);
 return idea;
 }
 
+//This function pushes new idea objects to the data model (savedIdeas)
 function pushIdea() {
     savedIdeas.push(idea);
     console.log(savedIdeas);
 }
+
+//This function makes a new element which is a card with the idea title and body 
+function renderCard() {
+        outputSection.innerHTML += `<div class="card"> 
+                                    <h4>${idea.title}</h4> 
+                                    <article>${idea.body}</article>    
+                                    </div>`
+}
+
+
+//A card should appear on the screen to match the comp above. The card should show the title and body the user entered into the form.
+//were going to for loop through the data model (savedIdeas)
+//for each element of the data model, we will create a new element within the output box, probably a div with a header and maybe p using the bodt tag
